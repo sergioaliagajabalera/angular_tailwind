805b5358ec7f0a4a9dd76405dc261a59028651d4
Sergio Aliaga Jabalera <74768958+sergioaliagajabalera@users.noreply.github.com>
2022-06-04 19:42:21 +0200

    add talwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: "", redirectTo: '/main',pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
